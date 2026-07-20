c375816c0249a6104a2bc306521f5f17c6381761
Ishaan Pai <ishaan.r.pai@gmail.com>
2026-07-20 09:33:49 +0530

    Update FIFO.v
    completed module implementation and fixed infinite loop bug

--- FIFO.v
+++ FIFO.v
@@ -19,20 +19,20 @@ module top #(
 	assign wr_ptr_grey = wr_ptr_bin ^ (wr_ptr_bin >> 1);
 	assign rd_ptr_grey = rd_ptr_bin ^ (rd_ptr_bin >> 1);
 	
-	wire [5:0] wr_ptr_grey_sync_1, wr_ptr_grey_sync_2;
-    wire [5:0] rd_ptr_grey_sync_1, rd_ptr_grey_sync_2;
+	reg [5:0] wr_ptr_grey_sync_1, wr_ptr_grey_sync_2;
+    reg [5:0] rd_ptr_grey_sync_1, rd_ptr_grey_sync_2;
 	
 	
 	// Update the read syncs when LCLK fires and update the write syncs when the RCLK fires
 	
 	always @(posedge LCLK) begin
 		rd_ptr_grey_sync_1 <= rd_ptr_grey;
-		rd_ptr_grey_sync_2 <= rd_ptr_grey_sync1;
+		rd_ptr_grey_sync_2 <= rd_ptr_grey_sync_1;
 	end
 	
 	always @(posedge RCLK) begin
 		wr_ptr_grey_sync_1 <= wr_ptr_grey;
-		wr_ptr_grey_sync2 <= wr_ptr_grey_sync1;
+		wr_ptr_grey_sync_2 <= wr_ptr_grey_sync_1;
 	end
 	
 	
@@ -48,7 +48,7 @@ module top #(
 	// Read data from the registers of the FIFO memory on the firing of the RCLK given that the reader has "Read Enabled" and the FIFO is not empty
 	
 	always @(posedge RCLK) begin
-			if (RDEN %% ~E) begin
+			if (RDEN && ~E) begin
 				RDDAT <= mem[rd_ptr_bin[4:0]];
 				rd_ptr_bin <= rd_ptr_bin + 1;
 			end
@@ -57,8 +57,12 @@ module top #(
 	
 	// Set the Full and Empty flags as per the read and write pointers
 	
-	always begin
+	always@(*) begin
+		// Full logic : first two MSBs match but the other two match
+		F = (wr_ptr_grey == {~rd_ptr_grey_sync_2[5:4], rd_ptr_grey_sync_2[3:0]});
 		
+		// Empty logic : read pointer equal to write pointer
+		E = rd_ptr_grey == wr_ptr_grey_sync_2; 
 	end
 	
 endmodule
