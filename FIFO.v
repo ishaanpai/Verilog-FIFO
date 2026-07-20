dcb6891d68c7398dded5a4c72eef5eae4c459924
Ishaan Pai <ishaan.r.pai@gmail.com>
2026-07-20 09:02:23 +0530

    Update FIFO.v
    added sync logic and WRDAT, RDDAT logic

--- FIFO.v
+++ FIFO.v
@@ -4,7 +4,7 @@ module top #(
 )(
     input RCLK, LCLK, WRDV, RDEN,
     input [width-1:0] WRDAT,
-    output reg F, AF, E, AE,
+    output reg F, E,
     output reg [width-1:0] RDDAT
 );
 
@@ -19,7 +19,46 @@ module top #(
 	assign wr_ptr_grey = wr_ptr_bin ^ (wr_ptr_bin >> 1);
 	assign rd_ptr_grey = rd_ptr_bin ^ (rd_ptr_bin >> 1);
 	
-	wire [5:0] wr_ptr_grey_sync_1, wr_ptr_grey_sync2;
+	wire [5:0] wr_ptr_grey_sync_1, wr_ptr_grey_sync_2;
     wire [5:0] rd_ptr_grey_sync_1, rd_ptr_grey_sync_2;
+	
+	
+	// Update the read syncs when LCLK fires and update the write syncs when the RCLK fires
+	
+	always @(posedge LCLK) begin
+		rd_ptr_grey_sync_1 <= rd_ptr_grey;
+		rd_ptr_grey_sync_2 <= rd_ptr_grey_sync1;
+	end
+	
+	always @(posedge RCLK) begin
+		wr_ptr_grey_sync_1 <= wr_ptr_grey;
+		wr_ptr_grey_sync2 <= wr_ptr_grey_sync1;
+	end
+	
+	
+	// Write data into the registers of the FIFO memory on the firing of the LCLK given that the source has "Write Valid" and the FIFO is not full
+	
+	always @(posedge LCLK) begin
+		if (WRDV && ~F) begin
+			mem[wr_ptr_bin[4:0]] <= WRDAT;
+			wr_ptr_bin <= wr_ptr_bin + 1;
+		end
+	end
+	
+	// Read data from the registers of the FIFO memory on the firing of the RCLK given that the reader has "Read Enabled" and the FIFO is not empty
+	
+	always @(posedge RCLK) begin
+			if (RDEN %% ~E) begin
+				RDDAT <= mem[rd_ptr_bin[4:0]];
+				rd_ptr_bin <= rd_ptr_bin + 1;
+			end
+	end
+	
+	
+	// Set the Full and Empty flags as per the read and write pointers
+	
+	always begin
+		
+	end
 	
 endmodule
